66daa5b33697f33fc5cb643158f62760f9189c75
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-03-02 00:19:08 +0500

    Database And Admin Panel

--- base/views.py
+++ base/views.py
@@ -1,27 +1,24 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {"id": 1, "name": "Lets Learn Python"},
-    {"id": 2, "name": "Lets Learn Js"},
-    {"id": 3, "name": "Lets Learn PHP"},
-    {"id": 4, "name": "Lets Learn C"},
-    {"id": 5, "name": "Lets Learn Java"}
-]
+# rooms = [
+#     {"id": 1, "name": "Lets Learn Python"},
+#     {"id": 2, "name": "Lets Learn Js"},
+#     {"id": 3, "name": "Lets Learn PHP"},
+#     {"id": 4, "name": "Lets Learn C"},
+#     {"id": 5, "name": "Lets Learn Java"}
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     home_data = {'rooms': rooms}
     return render(request, 'base/home.html', home_data)
 
 
 def room(request, pk):
-    found_room = None
-
-    for i in rooms:
-        if i['id'] == int(pk):
-            found_room = i
-
+    found_room = Room.objects.get(id=pk)
     room_data = {'room': found_room}
     return render(request, 'base/room.html', room_data)
