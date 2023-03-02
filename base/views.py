c459fbbd2c1648bf4dce032564b692e0d5e716a9
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-03-02 05:32:11 +0500

    User Registration

--- base/views.py
+++ base/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -19,12 +20,12 @@ from django.contrib.auth.decorators import login_required
 
 
 def loginPage(request):
-
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -38,13 +39,34 @@ def loginPage(request):
             login(request, user)
             return redirect('home')
 
-    context = {}
+    context = {"page": page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An Error Occured')
+
+    context = {"form": form}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
